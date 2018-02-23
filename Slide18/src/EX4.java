2a60d55cb30595e2854c7f83c3dec28cc4fc07be
palomara <palomarangelllhotmail.com>
2018-02-23 12:43:01 -0300

    first

---
+++ Slide18/src/EX4.java
@@ -0,0 +1,12 @@
+
+public class EX4 {
+	
+	public static void main (String [] args) {
+
+	String curso = "Curso de Java";
+	String exemplo = "Exemplo String";
+	
+	System.out.println ("" curso + exemplo);
+
+		}
+}
---
+++ Slide18/src/EX6.java
@@ -0,0 +1,14 @@
+
+public class EX6 {
+	public static void main (String [] args) {
+
+		String curso = "Curso de Java" + "-Exemplo String";
+
+		String pedaço = curso.split ("");
+
+		int i = curso.indexOf ("Exemplo String");
+
+		System.out.println (i);
+		}
+
+}
