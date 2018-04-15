89802107b3a8254999dab281f888389091439e9f
palomara <palomarangelll@hotmail.com>
2018-04-14 23:06:06 -0300

    interface

---
+++ PessoaFisica/src/model/PessoaFisica.java
@@ -0,0 +1,20 @@
+package model;
+
+public class PessoaFisica {
+	
+	private String nome;
+	private String end;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEnd() {
+		return end;
+	}
+	public void setEnd(String end) {
+		this.end = end;
+	}
+}
